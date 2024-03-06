7445f62cb2fc0a1772e31a41851b7522a667c25e
RugungDaimary <rugung4702@gmail.com>
2024-03-06 08:12:25 +0530

    Create DSU.cpp

---
+++ C++LB/DSU.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+//this DSU is by size,we can make DSU by rank and path compression also
+class DSU
+{
+    vector<int> parent;
+    vector<int> size;
+    int numberOfComponents;
+
+public:
+    DSU(int n)
+    {
+        parent.resize(n);
+        size.resize(n);
+        numberOfComponents = n;
+
+        for (int i = 0; i < n; i++)
+        {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int find(int x)
+    {
+        if (x == parent[x])
+            return x;
+
+        return parent[x] = find(parent[x]);
+    }
+
+    void Union(int x, int y)
+    {
+        int x_parent = find(x);
+        int y_parent = find(y);
+
+        if (x_parent == y_parent)
+            return;
+
+        if (size[x_parent] > size[y_parent])
+        {
+            parent[y_parent] = x_parent;
+            size[x_parent] += size[y_parent];
+        }
+        else if (size[x_parent] < size[y_parent])
+        {
+            parent[x_parent] = y_parent;
+            size[y_parent] += size[x_parent];
+        }
+        else
+        {
+            parent[x_parent] = y_parent;
+            size[y_parent] += size[x_parent];
+        }
+
+        numberOfComponents -= 1;
+    }
+
+    int maxSize()
+    {
+        return numberOfComponents;
+    }
+};
+int main()
+{
+
+    return 0;
+}

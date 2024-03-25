7e50c3e93664c3cb3c9b0d674d2dc8cc68e71a89
RugungDaimary <rugung4702@gmail.com>
2024-03-25 16:18:55 +0530

    changed

--- C++LB/practice.cpp
+++ C++LB/practice.cpp
@@ -1,28 +1,27 @@
-#include <iostream>
-#include <set>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 //Finding the position in set of an element;
-int position(set<int>&st,int element){
-    cout<<"Hii\n";
+int position(set<int> &st, int element)
+{
     auto it = lower_bound(st.begin(), st.end(), element);
     if (it != st.end() && *it == element)
     {
-        int pos=distance(st.begin(), it);//this itself takes O(n) time for calculating distance,we can use PBDS
+        int pos = distance(st.begin(), it); // this itself takes O(n) time for calculating distance,we can use PBDS
         return pos;
     }
     else
     {
-       return -1;
+        return -1;
     }
 }
 
+
 int main()
+
 {
-    // cout<<"Hii\n";
+  
    set<int>st={1,3,6,9};
-   int element = 9;
+   int element = 10;
 
    cout << position(st, element) << endl;
    st.erase(10);//is elemet is not present ,it does nothing ,no error

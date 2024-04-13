b03af4e9ce7e328e1c47177b3ba677709ba9c785
RugungDaimary <rugung4702@gmail.com>
2024-04-13 13:53:09 +0530

    updated

--- C++LB/PBDS.cpp
+++ C++LB/PBDS.cpp
@@ -2,10 +2,8 @@
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
-
 using namespace std;
 using namespace __gnu_pbds;
-
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // increasing
                                                                                               //  greater<int> //for decreasing
 // for multi_set ,use less_equal<int>

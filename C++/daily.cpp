40bb9a16e4149d1704145b6823e7d06beafda7c9
RugungDaimary <rugung4702@gmail.com>
2024-06-22 18:36:49 +0530

    ok

--- C++/daily.cpp
+++ C++/daily.cpp
@@ -1,6 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    cout<<"HII";
+    vector<int>nums={1,3,6,8,9};
+    int sum=accumulate(nums.begin()+2,nums.end(),0);
+    cout<<sum<<endl;
+    
     return 0;
 }

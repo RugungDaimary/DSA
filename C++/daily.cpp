da86e1c4c5bf65d3137d3d0dded4bcb7b66a7c5a
RugungDaimary <rugung4702@gmail.com>
2024-08-23 07:46:21 +0530

    added

--- C++/daily.cpp
+++ C++/daily.cpp
@@ -1,61 +1,31 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int solve(int n, vector<vector<int>> &adj)
-{
-    vector<int> indegree(n, 0);
-    for (int u = 0; u < n; u++)
+class TrieNode{
+    public:
+    char data;
+    vector<TrieNode*>child;
+    bool isTerminal;
+    TrieNode(char d) : data(d), child(26, nullptr),isTerminal(false)
     {
-        for (auto v : adj[u])
-        {
-            indegree[v]++;
-        }
     }
-    queue<int> q;
-    for (int i = 0; i < n; i++)
-    {
-        if (indegree[i] == 0)
-        {
-            q.push(i);
+    void insert(int idx,string &s,TrieNode*root){
+        int n=s.length();
+        if(idx>=n){
+            return;
         }
-    }
-    int cnt = 1;
-    vector<int> topoSorted;
-    while (!q.empty())
-    {
-        int sz = q.size();
-        cnt *= sz;
-        while (sz--)
-        {
-            int fNode = q.front();
-            topoSorted.push_back(fNode);
+        char ch=s[idx]-'a';
 
-            q.pop();
-            for (auto nbr : adj[fNode])
-            {
-                indegree[nbr]--;
-                if (indegree[nbr] == 0)
-                {
-                    q.push(nbr);
-                }
-            }
-        }
-    }
-    if (topoSorted.size() != n)
-        return 0;
-    return cnt;
-}
-int main()
-{
-    int n = 10;
-    vector<vector<int>> edges{{0, 4}, {4, 7}, {7, 9}, {1, 4}, {4, 7}, {2, 5}, {5, 7}, {3, 6}, {6, 8}, {8, 9}};
-    vector<vector<int>> adj(n);
-    for (auto edge : edges)
-    {
-        int u = edge[0];
-        int v = edge[1];
-        adj[v].push_back(u);
-    }
-    cout << solve(n, adj) << endl;
 
+
+
+    }
+    bool check(int idx,string &s, TrieNode *root)
+    {
+
+    }
+};
+
+int main(){
+    
     return 0;
 }

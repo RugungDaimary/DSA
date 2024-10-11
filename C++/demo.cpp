a7dcffbc17f95b23ab95bb70ad20fafd76c45cbf
RugungDaimary <rugung4702@gmail.com>
2024-10-12 01:02:37 +0530

    demo

--- C++/demo.cpp
+++ C++/demo.cpp
@@ -1,5 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
+int binarySearch(vector<pair<int,int>>&v,pair<int,int>&target){
+    int n=v.size();
+    int low=0;
+    int high=n;                                                     
+    int index=n;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(times[mid][0])
+    }
+
+}
 int main(){
     
     return 0;
